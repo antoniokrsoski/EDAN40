e42169eea533d3aae2e500272879ba7315ed3dbd
Antonio Krsoski <antonio.krsoski.0473@student.lu.se>
2023-03-24 16:37:39 +0100

    task 2

--- Labs/Lab1/Soduku.hs
+++ Labs/Lab1/Soduku.hs
@@ -3,11 +3,9 @@ module Sudoku where
 import Data.Char (digitToInt)
 import Distribution.Simple.Utils (xargs)
 
-rows = "ABCD"
+rows = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-cols = "1234"
-
--- Task 1
+cols = "123456789"
 
 -- Eq is a class that defines equality, making sure that the type is comparable
 -- a is the type of the element
@@ -18,9 +16,13 @@ containsElem elem (x : xs)
   | elem == x = True
   | otherwise = containsElem elem xs
 
+-- Task 0
+
 cross :: [a] -> [a] -> [[a]]
 cross a b =
   [[x, y] | x <- a, y <- b]
+
+-- Task 1
 
 convertZero :: Char -> Char
 convertZero a
@@ -32,6 +34,7 @@ convert [] = []
 convert (x : xs) = convertZero x : convert xs
 
 -- Task 2
+-- Only 4x4 and 9x9 since I don't know how to solve the cols list with more than values up to 9
 
--- board :: [Int] -> [(String, Int)]
--- board i = cross rows [1 .. i]
+board :: Int -> [String]
+board n = cross (take n rows) (take n cols)
