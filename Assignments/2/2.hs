ae375138ea30de6326f9f719d61df6586138b068
Antonio Krsoski <antonio.krsoski.0473@student.lu.se>
2023-05-10 14:10:47 +0200

    done

--- Assignments/2/2.hs
+++ Assignments/2/2.hs
@@ -39,6 +39,16 @@ maximaBy valueFcn xs = filter (\x -> valueFcn x == maxValue) xs
   where
     maxValue = maximum (map valueFcn xs)
 
+-- Case 1: Empty list of list tuples
+-- Case 2: At least one element in list1 and empty list2 ->
+--  attach x as head of first list and '-' as head of second list. The lists
+--  being returned from recursive call of optAlignments on remaining list contents.
+-- Case 3: Same as 2 except first is empty and second is not.
+-- Case 4: Both lists have contents -> Create all alignments by concatenating
+--  the result of attaching both heads to respective list, attaching x as head for first and '-' as head for second
+--  and vice versa. Then perform maximaBy on these three different results to get the
+--  best one.
+
 optAlignments :: String -> String -> [AlignmentType]
 optAlignments [] [] = [([], [])]
 optAlignments (x : xs) [] = attachHeads x '-' (optAlignments xs [])
@@ -53,38 +63,57 @@ optAlignments (x : xs) (y : ys) = maximaBy sim alignments
           attachHeads '-' y (optAlignments (x : xs) ys)
         ]
 
-
 outputOptAlignments :: String -> String -> IO ()
 outputOptAlignments string1 string2 = do
-    putStrLn("There are " ++ show (length (optAlignments string1 string2)) ++ " optimal alignments: \n") 
-    putStrLn("These are the optimal alignments " ++ show (optAlignments string1 string2))
+    putStrLn("There are " ++ show (length (optAlignmentsOptimization string1 string2)) ++ " optimal alignments: \n") 
+    putStrLn("These are the optimal alignments " ++ show (optAlignmentsOptimization string1 string2))
 
+similarityScoreOptimization :: String -> String -> Int
+similarityScoreOptimization xs ys = simScore (length xs) (length ys)
+    where
+        simScore :: Int -> Int -> Int
+        simScore i j = grid !! i !! j
 
-mcsLength :: Eq a => [a] -> [a] -> Int
-mcsLength xs ys = mcsLen (length xs) (length ys)
-  where
-    mcsLen i j = mcsTable!!i!!j
-    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
-       
-    mcsEntry :: Int -> Int -> Int
-    mcsEntry _ 0 = 0
-    mcsEntry 0 _ = 0
-    mcsEntry i j
-      | x == y    = 1 + mcsLen (i-1) (j-1)
-      | otherwise = max (mcsLen i (j-1)) 
-                        (mcsLen (i-1) j)
-      where
-         x = xs!!(i-1)
-         y = ys!!(j-1)
+        grid :: [[Int]]
+        grid = [[entry i j | j <- [0..]] | i <- [0..]]
 
+        entry :: Int -> Int -> Int
+        entry 0 0 = 0
+        entry i 0 = i * scoreSpace
+        entry 0 j = j * scoreSpace
+        entry i j = maximum [simScore (i - 1) (j - 1) + score (x, y),
+                             simScore (i - 1)  j      + score (x, '-'),
+                             simScore  i      (j - 1) + score ('-', y)]
+            where
+                x = xs !! (i - 1)
+                y = ys !! (j - 1)
 
+optAlignmentsOptimization :: String -> String -> [AlignmentType]
+optAlignmentsOptimization xs ys = map
+                          (pairApply reverse)
+                          (snd $ alignment (length xs) (length ys))
+    where
+        pairApply f (a, b) = (f a, f b)
 
--- Case 1: Empty list of list tuples
--- Case 2: At least one element in list1 and empty list2 ->
---  attach x as head of first list and '-' as head of second list. The lists
---  being returned from recursive call of optAlignments on remaining list contents.
--- Case 3: Same as 2 except first is empty and second is not.
--- Case 4: Both lists have contents -> Create all alignments by concatenating
---  the result of attaching both heads to respective list, attaching x as head for first and '-' as head for second
---  and vice versa. Then perform maximaBy on these three different results to get the
---  best one.
+        alignment :: Int -> Int -> (Int, [AlignmentType])
+        alignment i j = grid !! i !! j
+
+        grid :: [[(Int, [AlignmentType])]]
+        grid = [[entry i j | j <- [0..]] | i <- [0..]]
+
+        entry :: Int -> Int -> (Int, [AlignmentType])
+        entry 0 0 = (0, [([], [])])
+        entry i 0 = (i * scoreSpace, [(take i xs, replicate i '-')])
+        entry 0 j = (j * scoreSpace, [(replicate j '-', take j ys)])
+        entry i j = (fst $ head opt, concat [snd b | b <- opt])
+            where 
+                (s1, a1) = alignment (i - 1) (j - 1)
+                (s2, a2) = alignment (i - 1)  j
+                (s3, a3) = alignment  i      (j - 1)
+                x = xs !! (i - 1)
+                y = ys !! (j - 1)
+                opt = maximaBy fst $ [
+                    (s1 + score (x,y),   attachHeads x y   a1),
+                    (s2 + score (x, '-'), attachHeads x '-' a2),
+                    (s3 + score ('-', y), attachHeads '-' y a3)]
+
